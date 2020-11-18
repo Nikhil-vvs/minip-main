db088c97ec76e178e65b8470e281b8bc376e045e
Nikhil-vvs <nikhilvvs2000@gmail.com>
2020-11-18 20:33:30 +0530

    nikhil changed

---
+++ minip-main/src/app/welcome/welcome.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage implements OnInit {
+
+  constructor(private nav: NavController) { }
+
+  ngOnInit() {
+  }
+
+  gotoLoginpage(){
+    this.nav.navigateForward(['login']);
+  }
+
+  gotoSignuppage(){
+    this.nav.navigateForward(['register']);
+  }
+}
